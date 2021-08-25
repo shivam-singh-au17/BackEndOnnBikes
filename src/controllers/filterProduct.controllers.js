2b23fad9934f276ef0be2cc7b8bd6440e2a69c1a
Shivam Singh <shivamsingh4458@gmail.com>
2021-08-25 18:50:15 +0530

    adding allfilter page

--- src/controllers/filterProduct.controllers.js
+++ src/controllers/filterProduct.controllers.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 const Filterprod = require("../models/filterProduct.model");
-const crudController = require("./crud.controller");
+const crudController = require("./crudFilter.controllers");
 
 router.post("/", crudController(Filterprod).post);
 router.get("/", crudController(Filterprod).get);
--- src/server.js
+++ src/server.js
@@ -7,7 +7,10 @@ const filterprodController = require("./controllers/filterProduct.controllers");
 
 const app = express();
 app.use(express.json());
+app.use("/static", express.static(path.join(__dirname, "public/")));
 
+app.set("views", path.join(__dirname, "views/"));
+app.set("view engine", "ejs");
 
 app.use("/filterProducts", filterprodController);
 
