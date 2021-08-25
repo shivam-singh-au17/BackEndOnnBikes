42cf305507dbbcc1e806cd5e52c91b966b9f2f80
Shivam Singh <shivamsingh4458@gmail.com>
2021-08-25 22:58:20 +0530

    update

--- src/server.js
+++ src/server.js
@@ -4,12 +4,19 @@ const express = require("express");
 
 const connect = require("./configs/db");
 const filterprodController = require("./controllers/filterProduct.controllers");
+const arroflocationController = require("./controllers/arrofLocation.controller");
+const bikesobjectController = require("./controllers/bikesobject.controller");
 
 const app = express();
 app.use(express.json());
+app.use("/static", express.static(path.join(__dirname, "public/")));
 
+app.set("views", path.join(__dirname, "views/"));
+app.set("view engine", "ejs");
 
 app.use("/filterProducts", filterprodController);
+app.use("/arrofLocation", arroflocationController);
+app.use("/bikesObject", bikesobjectController);
 
 app.listen(3007, async () => {
     await connect();
