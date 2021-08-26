6042ebdb3bb09ce95a1a2a89acefabb83657d4fd
Shivam Singh <shivamsingh4458@gmail.com>
2021-08-26 12:07:52 +0530

    server files update

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 
 const path = require("path");
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const connect = require("./configs/db");
 const filterprodController = require("./controllers/filterProduct.controllers");
@@ -9,12 +10,15 @@ const bikesobjectController = require("./controllers/bikesobject.controller");
 
 const app = express();
 app.use(express.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 app.use("/static", express.static(path.join(__dirname, "public/")));
 
 app.set("views", path.join(__dirname, "views/"));
 app.set("view engine", "ejs");
 
-app.use("/filterProducts", filterprodController);
+app.use("/safety", filterprodController);
 app.use("/arrofLocation", arroflocationController);
 app.use("/bikesObject", bikesobjectController);
 
